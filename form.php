9a51f1e38961ba0577c714e58d8ec5a7bc3173fe
python-programing <79223364+python-coding-404@users.noreply.github.com>
2022-04-16 15:54:01 +0400

    Add files via upload

---
+++ form.php
@@ -0,0 +1,43 @@
+<html>
+	<head>
+	<link rel="stylesheet" href="http://localhost/style.css">
+         <title>NAVRBARA</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+	</head>
+	<body>
+<header>
+<nav class="bg-light">
+<li>
+<p>Php</p>
+<li>
+<a href="http://localhost/"> Home </a>
+</li>
+<li> <a href="http://localhost/gpl-3.0.html"><i>Licence</i></a>
+</li>
+</ul>
+</nav>
+</header>
+
+
+
+ 
+		
+		<h1>This Website is Just an form</h2>
+		<h1>Your name is <?php echo $_POST["fist"]; ?> <?php echo $_POST["sec"];?> </h1>
+	
+
+
+               
+			
+			
+			
+			
+			
+			
+			
+	
+	
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC
+    "-//W3C//DTD XHTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" >
+	<head>
+	<link rel="stylesheet" href="http://localhost/style.css">
+	 <title>NAVRBARA</title>
+	<link rel="stylesheet" href="/forms.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+	</head>
+	<body>
+<header>
+<nav class="bg-light">
+<li>
+<p>Php</p>
+<li>  
+<a href="http://localhost/"> Home </a>  
+</li>   
+<li> <a href="http://localhost/gpl-3.0.html"><i>Licence</i></a>  
+</li>  
+</ul>  
+</nav>  
+</header>
+
+
+
+ 
+		
+		<h2>This Website is Just an form</h2>
+		<h1>Welcome to PHP!</h1>
+		<h1 class="font-italic">Hlelo</h1>`
+		<form action="http://localhost/form.php" method="POST">
+			<label for="fisrt" >First Name:</label>
+			<input type="text" id="fisrt" name="fist" class="form-control" required placeholder="John"> </input>
+			<label for="sec">Second Name:</label>
+			<input type="text" id="sec" name="sec" class="form-control" required placeholder="Doe"> </input>
+			<input type="submit" class="button" style="background-color: var(--bs-blue)"></input></form>
+		<img src="http://localhost/info.jpg" width="50" height="50" >
+
+			
+			
+			
+			
+			
+			
+<div role="button" class="button">LO</div>	
+	
+	</body>
+<script type="text/javascript" src="https://rawgit.com/socialwolves/easter-egg/master/index.js"></script>
+</html>
